f398ae50771227d6ac9ac64219b09aa16560890a
Wood-Benjamin <38670310+Wood-Benjamin@users.noreply.github.com>
2019-10-16 21:35:05 -0600

    Initial commit

---
+++ src/myhttpurlconnection/Myhttpurlconnection.java
@@ -0,0 +1,39 @@
+package myhttpurlconnection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Myhttpurlconnection {
+
+    private static Scanner in;
+
+    public static void main(String args[]) throws IOException {
+        in = new Scanner(System.in);
+        String firstname = null;
+        String lastname = null;
+        System.out.println("Please enter you firstname: ");
+        firstname = in.next();
+
+        System.out.println("Please enter your lastname: ");
+        lastname = in.next();
+
+        String myphplink = "http://localhost:10080/java/myhttpurlconnect.php?firstname="+firstname+"&lastname="+lastname;
+        URL url = new URL(myphplink);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while((line = in.readLine()) != null){
+            sb.append(line);
+        }
+        in.close();
+        System.out.println(sb.toString());
+    }
+}
